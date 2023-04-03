c448da0ccda857c8d62c778b737d1f8a3d655a86
okinyoCoder <okinyoanthony@gmail.com>
2023-04-03 18:08:19 +0000

    removed trailing whitespace

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Module representing class BaseModel that defines all 
-common attributes/methods for other classes
+"""Module representing class BaseModel that defines all
+common attributes methods for other classes
 """
+
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Base class from which other classes can inherite"""
-
     def __init__(self):
         """class constructor method for creating objects
         Args:
              id (str): assign with an uuid when an instance is created
-             created_at (date): assign with the current datetime when an 
+             created_at (date): assign with the current datetime when an
                                instance is created
-             updated_at (date): assign with the current datetime when an 
-                               instance is created and it will be updated 
-                               every time you change your object
+             updated_at (date): assign with the current datetime when an
+                                instance is created and it will be updated
+                                every time you change your object
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -29,18 +30,18 @@ class BaseModel:
         Returns:
                 return formatted string if succesful
         """
-        
-        return "[{}] {} {}".format(self.__class__.__name__,  self.id, self.__dict__)
+        return
+        "[{}] {} {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Method that pdates the public instance attribute updated_at 
+        """Method that pdates the public instance attribute updated_at
         with the current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """Method returns a dictionary containing all keys/values of
-         __dict__ of the instance
+        __dict__ of the instance
         """
         my_dict = {}
         my_dict["__class__"] = self.__class__.__name__
