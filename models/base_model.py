8303669c1a3c94dc19efbfa9b185a91c1c774b50
okinyoCoder <okinyoanthony@gmail.com>
2023-04-05 07:54:23 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 class BaseModel:
     """Base class from which other classes can inherite"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """class constructor method for creating objects
         Args:
              id (str): assign with an uuid when an instance is created
@@ -18,10 +18,23 @@ class BaseModel:
              updated_at (date): assign with the current datetime when an
                                 instance is created and it will be updated
                                 every time you change your object
+             *args : variable length argument list
+             **kwargs : Arbitrary keyword arguement
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
+                else:
+                    if key == "created_at" or key_inside == "updated_at":
+                        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                        datetime_object = datetime.strptime(
+                                value, date_format)
+                        setattr(self, key, datetime_object)
 
     def __str__(self):
         """Method that prints string representation of Basemodel
